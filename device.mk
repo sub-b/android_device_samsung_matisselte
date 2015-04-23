647a2b2cd89f23f280406030dcfe9a7a0bacc555
sub77 <sub77@ymail.com>
2015-04-23 19:51:00 +0200

    x

--- device.mk
+++ device.mk
@@ -123,6 +123,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
 
+# SamsungServiceMode
+PRODUCT_PACKAGES += \
+    SamsungServiceMode
 
 # Default.prop overrides to get adb working at boot
 ADDITIONAL_DEFAULT_PROPERTIES += \
